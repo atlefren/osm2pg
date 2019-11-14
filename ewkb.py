57c5d819f32b175e4bae0584d7a63f565319b156
Atle Frenvik Sveen <atle.sveen@gmail.com>
2019-11-14 11:43:29 +0100

    error handling

--- ewkb.py
+++ ewkb.py
@@ -3,7 +3,10 @@ from lgeos import readWkbHex, writeEWkbHex
 
 
 def add_srid(wkb, srid=4326):
-    geom = readWkbHex(wkb)
-    ewkt = writeEWkbHex(geom, srid)
-
-    return ewkt
+    try:
+        geom = readWkbHex(wkb)
+        ewkt = writeEWkbHex(geom, srid)
+        return ewkt
+    except MemoryError as e:
+        print(e)
+        raise e
